39608dbc295986b06290e8bfb9fd4752a9e60976
gnmaxim <maximgaina@yandex.ru>
2017-11-21 13:37:49 +0100

    environment setup

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: ['./src/js/app.js'],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.js'
+  },
+  plugins: [
+    // Copy our app's index.html to the build folder.
+    new CopyWebpackPlugin([
+      { from: './src/index.html', to: "index.html" }
+    ])
+  ],
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' }
+    ]
+  }
+};
